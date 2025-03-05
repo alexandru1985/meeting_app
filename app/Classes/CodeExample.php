34026a3f3cce48a9c20d20469656489d0fd1efaf
alexandru1985 <alexandru_panzaru@yahoo.com>
2025-03-05 18:42:53 +0200

    Add tables

--- app/Classes/CodeExample.php
+++ app/Classes/CodeExample.php
@@ -3,3 +3,15 @@
 $location = Location::find(1);
 $events = $location->events;
 $tables = $location->tables;
+
+attendees
+    id - integer
+    name - string
+
+events
+    id - integer
+    name - string
+
+attendee_event
+    attendee_id - integer
+    event_id - integer
